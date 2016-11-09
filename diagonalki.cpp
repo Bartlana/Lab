3fa3326045bba2a870bbbbfbe3a18ddc026bfd6b
Katherine08 <dudarec08@gmail.com>
2016-11-09 23:29:36 +0200

    Update diagonalki.cpp

--- diagonalki.cpp
+++ diagonalki.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int main() {
-	int n, i, j, s=0;
+	int n, i, j, s=0,s1=0;
 	cin>>n;
 	int a[n][n];
 	for (i=0; i<n; i++)
@@ -13,8 +13,11 @@ int main() {
 			for (j=0; j<n; j++){
 				if (i==j)
 				s=s+a[i][j];
+				if (i==n-j-1)
+					s1+=a[i][j];
+			
 			}
 		}
-			cout<<s;
+			cout<<s<<" "<<s1;
 	return 0;
 }
